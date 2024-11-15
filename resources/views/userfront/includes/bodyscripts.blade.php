38adeeded721f2ed0e8b04abe922c1750be76936
rashed <125754285+Rashed-ux920@users.noreply.github.com>
2024-11-15 19:27:17 +0300

    manege nav bar

---
+++ resources/views/userfront/includes/bodyscripts.blade.php
@@ -0,0 +1,12 @@
+  <!-- Vendor JS Files -->
+  <script src="{{asset('frontuser/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('frontuser/assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{asset('frontuser/assets/vendor/aos/aos.js')}}"></script>
+  <script src="{{asset('frontuser/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('frontuser/assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
+  <script src="{{asset('frontuser/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('frontuser/assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
+  <script src="{{asset('frontuser/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+
+  <!-- Main JS File -->
+  <script src="{{asset('/frontuser/assets/js/main.js')}}"></script>
---
+++ resources/views/userfront/includes/headlinks.blade.php
@@ -0,0 +1,21 @@
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+<link href="{{asset('/frontuser/assets/img/favicon.png')}}" rel="icon">
+<link href="{{asset('/frontuser/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com" rel="preconnect">
+<link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="{{asset('/frontuser/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('/frontuser/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+<link href="{{asset('/frontuser/assets/vendor/aos/aos.css')}}" rel="stylesheet">
+<link href="{{asset('/frontuser/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+<link href="{{asset('/frontuser/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+
+<!-- Main CSS File -->
+<link href="{{asset('/frontuser/assets/css/main.css')}}" rel="stylesheet">
